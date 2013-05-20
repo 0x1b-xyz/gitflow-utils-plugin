9abb81d895cac4cee6423c3cfa81080a279c2534
kphonik <jason@stiefel.io>
2013-05-20 09:56:42 -0400

    Correctly checking the branch out now

--- src/main/java/org/jenkinsci/plugins/gitflowutils/promotion/CheckoutGitBranch.java
+++ src/main/java/org/jenkinsci/plugins/gitflowutils/promotion/CheckoutGitBranch.java
@@ -24,7 +24,7 @@ import java.util.Map;
 import java.util.logging.Logger;
 
 /**
- * Checks the promoted {@link hudson.plugins.git.GitSCM#GIT_BRANCH} into a randomized directory.
+ * Checks the promoted {@link hudson.plugins.git.GitSCM#GIT_BRANCH} in the workspace.
  *
  * @author jason@stiefel.io
  */
@@ -53,25 +53,22 @@ public class CheckoutGitBranch extends Builder {
 
         FilePath workspace = build.getWorkspace();
 
-        listener.getLogger().println("Clearing workspace at " + workspace.getRemote());
-        workspace.deleteContents();
-
         GitSCM scm = (GitSCM)build.getTarget().getProject().getScm();
         final String gitExe = scm.getGitExe(build.getBuiltOn(), listener);
 
-        listener.getLogger().println("Checking out " + gitBranch + " into " + workspace.getRemote());
+        listener.getLogger().println("[gitflow-utils] Checking out " + gitBranch + " into " + workspace.getRemote());
 
         workspace.act(new FilePath.FileCallable<Void>() {
             public Void invoke(File workspace, VirtualChannel channel) throws IOException, InterruptedException {
 
                 GitClient git = Git.with(listener, environment).in(workspace).using(gitExe).getClient();
-                git.checkoutBranch("release/0.1", "HEAD");
+                git.checkoutBranch(gitBranch, "HEAD");
 
                 return null;
             }
         });
 
-        listener.getLogger().println("Complete");
+        listener.getLogger().println("[gitflow-utils] Complete");
 
         return true;
     }
